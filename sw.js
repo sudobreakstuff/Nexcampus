941366b15ce956b0e3aeb253219d0ab9401cf7dc
sudobreakstuff <shahidsingh1zn@gmail.com>
2026-05-23 13:54:52 +0200

    fix: align-items flex-start, PWA install always visible, SW clients.claim

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE = 'nexcampus-v2';
+const CACHE = 'nexcampus-v3';
 const ASSETS = [
   '.',
   'index.html',
+  'manifest.json',
   'static/css/retro.css',
   'static/js/utils.js',
   'static/js/app.js',
   'static/js/modules/notebook.js',
   'static/js/modules/notes.js',
   'static/js/modules/tools.js',
-  'manifest.json',
   'static/icons/icon-192.png',
   'static/icons/icon-512.png',
   'static/icons/favicon.png',
@@ -25,11 +25,14 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); })
-      );
-    })
+    Promise.all([
+      caches.keys().then(function(keys) {
+        return Promise.all(
+          keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); })
+        );
+      }),
+      self.clients.claim()
+    ])
   );
 });
 
