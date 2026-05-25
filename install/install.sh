c6775de43e1c39e331430cfbcbddd5acded5a4f1
sudobreakstuff <shahidsingh1zn@gmail.com>
2026-05-25 10:33:03 +0200

    bump install fallback to v2.4.7

--- install/install.sh
+++ install/install.sh
@@ -30,8 +30,8 @@ VERSION=$(echo "$LATEST" | python3 -c "import sys,json; print(json.load(sys.stdi
 
 if [ -z "$URL" ]; then
   echo "Warning: Could not find latest release. Using fallback..."
-  URL="https://github.com/$REPO/releases/download/v2.4.6/$BINARY"
-  VERSION="v2.4.6"
+  URL="https://github.com/$REPO/releases/download/v2.4.7/$BINARY"
+  VERSION="v2.4.7"
 fi
 
 echo "==> Downloading NexCampus $VERSION..."
