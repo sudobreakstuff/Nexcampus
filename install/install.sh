0764f87f9f004bc1a98d337f3fba34ab956f165e
sudobreakstuff <shahidsingh1zn@gmail.com>
2026-05-25 13:41:36 +0200

    v2.8.6: removed diagnostic code that was falsely reporting missing functions

--- install/install.sh
+++ install/install.sh
@@ -30,8 +30,8 @@ VERSION=$(echo "$LATEST" | python3 -c "import sys,json; print(json.load(sys.stdi
 
 if [ -z "$URL" ]; then
   echo "Warning: Could not find latest release. Using fallback..."
-  URL="https://github.com/$REPO/releases/download/v2.8.5/$BINARY"
-  VERSION="v2.8.5"
+  URL="https://github.com/$REPO/releases/download/v2.8.6/$BINARY"
+  VERSION="v2.8.6"
 fi
 
 echo "==> Downloading NexCampus $VERSION..."
